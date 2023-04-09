13cde1523a6b026f08d70158e3dbcea57fa915ea
rashadulalam <rashadul91@gmail.com>
2023-04-09 20:14:48 +0600

    Motion effect added to Banner section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,21 +2,31 @@ import React from 'react';
 import Image from '../assets/avatar.svg';
 import {FaGithub, FaYoutube, FaDribbble} from 'react-icons/fa';
 import { TypeAnimation } from 'react-type-animation';
-// import {motion} from 'framer-motion';
-// import {fadeIn} from '../variants';
+import { motion } from 'framer-motion';
+import {fadeIn} from '../variants';
 
 
 const Banner = () => {
   return (
     <>
-      <section className='section' id='home'>
+      <section className='min-h-[85vh] lg:min-h[78vh] flex items-center' id='home'>
         <div className='container mx-auto'>
           <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
           <div className='flex-1 text-center font-secondary lg:text-left'>
-            <h1 className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
+            <motion.h1 
+            variants={fadeIn('up', 0.3)} 
+            initial="hidden" 
+            whileInView="show" 
+            viewport={{once: false, amount: 0.7}} 
+            className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
               BEN <span>AIDEN</span>
-            </h1>
-            <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+            </motion.h1>
+            <motion.div 
+            variants={fadeIn('up', 0.3)} 
+            initial="hidden" 
+            whileInView="show" 
+            viewport={{once: false, amount: 0.7}}
+            className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
               <span className='mr-4'>I am a</span>
               <TypeAnimation sequence={[
                 'Developer', 
@@ -31,13 +41,32 @@ const Banner = () => {
                 wrapper='span'
                 repeat={Infinity} />
                 
-            </div>
-            <p className='mb-8 max-w-lg mx-auto lg:mx-0'>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
-            <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+            </motion.div>
+            <motion.p 
+
+            variants={fadeIn('up', 0.5)} 
+            initial="hidden" 
+            whileInView="show" 
+            viewport={{once: false, amount: 0.7}}
+
+            className='mb-8 max-w-lg mx-auto lg:mx-0'>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</motion.p>
+            <motion.div
+            variants={fadeIn('up', 0.6)} 
+            initial="hidden" 
+            whileInView="show" 
+            viewport={{once: false, amount: 0.7}}
+            className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
               <button className='btn btn-lg'>Contact me</button>
               <a href='/' className='text-gradient btn-link'>My Portfolio</a>
-            </div>
-            <div className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+            </motion.div>
+            <motion.div 
+            
+            variants={fadeIn('up', 0.7)} 
+            initial="hidden" 
+            whileInView="show" 
+            viewport={{once: false, amount: 0.7}}
+            
+            className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
               <a href='/'>
                 <FaYoutube />
               </a>
@@ -47,11 +76,17 @@ const Banner = () => {
               <a href='/'>
                 <FaDribbble />
               </a>
-            </div>
+            </motion.div>
           </div>
-          <div>
+          <motion.div 
+          variants={fadeIn('down', 0.5)} 
+          initial="hidden" 
+          whileInView="show" 
+          viewport={{once: false, amount: 0.7}}
+
+          className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'>
             <img src={Image} alt='' />
-          </div>
+          </motion.div>
           </div>
         </div>
       </section>
